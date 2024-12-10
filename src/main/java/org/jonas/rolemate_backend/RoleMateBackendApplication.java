225d4560794815495985e688787b518bfce3fdbd
Jonas Ullvahn <ullvahn@gmail.com>
2024-12-10 17:43:24 +0100

    Initial Commit

---
+++ src/main/java/org/jonas/rolemate_backend/RoleMateBackendApplication.java
@@ -0,0 +1,13 @@
+package org.jonas.rolemate_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleMateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoleMateBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jonas/rolemate_backend/RoleMateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jonas.rolemate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleMateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
