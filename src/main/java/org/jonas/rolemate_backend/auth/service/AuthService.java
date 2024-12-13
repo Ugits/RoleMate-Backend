f5714b65e748716d0430f069bad1a1fd33dfdd25
Jonas Ullvahn <ullvahn@gmail.com>
2024-12-13 18:17:35 +0100

    Add method to verify authentication and create token

--- src/main/java/org/jonas/rolemate_backend/auth/service/AuthService.java
+++ src/main/java/org/jonas/rolemate_backend/auth/service/AuthService.java
@@ -1,9 +1,14 @@
 package org.jonas.rolemate_backend.auth.service;
 
+import org.jonas.rolemate_backend.auth.dto.AuthRequest;
+import org.jonas.rolemate_backend.auth.dto.AuthResponse;
 import org.jonas.rolemate_backend.auth.jwt.JWTService;
 import org.jonas.rolemate_backend.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +28,28 @@ public class AuthService {
         this.userRepository = userRepository;
     }
 
+    public AuthResponse verify(AuthRequest authRequest) {
+        Authentication authentication =
+                authenticationManager.authenticate(
+                        new UsernamePasswordAuthenticationToken(
+                                authRequest.username(),
+                                authRequest.password()
+                        ));
 
+        String generatedToken = jwtService.generateToken(authRequest.username());
+        System.out.println("Generated Token: " + generatedToken);
+
+        return
+                new AuthResponse(
+                        generatedToken,
+                        authentication.getAuthorities()
+                                .stream()
+                                .map(GrantedAuthority::getAuthority)
+                                .filter(authority -> authority.startsWith("Role"))
+                                .findFirst()
+                                .orElseThrow(() -> new IllegalStateException("User has no role"))
+                                .substring(5)
+                );
+    }
 
 }
