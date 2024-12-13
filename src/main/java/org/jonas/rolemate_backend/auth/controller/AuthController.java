e54f527c21935ce56ec0c9089c9bf8ee14cae273
Jonas Ullvahn <ullvahn@gmail.com>
2024-12-13 17:56:54 +0100

    Add Auth Controller && Service

---
+++ src/main/java/org/jonas/rolemate_backend/auth/controller/AuthController.java
@@ -0,0 +1,19 @@
+package org.jonas.rolemate_backend.auth.controller;
+
+import org.jonas.rolemate_backend.auth.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+}
---
+++ src/main/java/org/jonas/rolemate_backend/auth/service/AuthService.java
@@ -0,0 +1,8 @@
+package org.jonas.rolemate_backend.auth.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+}
