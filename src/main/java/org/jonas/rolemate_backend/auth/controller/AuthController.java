340ead1f9c37ee5986d79c776153c47b66c32f60
Jonas Ullvahn <ullvahn@gmail.com>
2024-12-13 18:26:17 +0100

    Add login endpoint in authController

--- src/main/java/org/jonas/rolemate_backend/auth/controller/AuthController.java
+++ src/main/java/org/jonas/rolemate_backend/auth/controller/AuthController.java
@@ -1,7 +1,12 @@
 package org.jonas.rolemate_backend.auth.controller;
 
+import org.jonas.rolemate_backend.auth.dto.AuthRequest;
+import org.jonas.rolemate_backend.auth.dto.AuthResponse;
 import org.jonas.rolemate_backend.auth.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +19,11 @@ public class AuthController {
     @Autowired
     public AuthController(AuthService authService) {
         this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest authRequest) {
+        return ResponseEntity.ok(authService.verify(authRequest));
     }
 
 }
