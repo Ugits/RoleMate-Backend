ca4c3768995a3349ff526eb5255f3f32acf13404
Jonas Ullvahn <ullvahn@gmail.com>
2024-12-22 04:29:15 +0100

    Add Character Controller

---
+++ src/main/java/org/jonas/rolemate_backend/character/controller/CharacterController.java
@@ -0,0 +1,19 @@
+package org.jonas.rolemate_backend.character.controller;
+
+import org.jonas.rolemate_backend.character.CharacterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/character")
+public class CharacterController {
+
+    private final CharacterService characterService;
+
+    @Autowired
+    public CharacterController(CharacterService characterService) {
+        this.characterService = characterService;
+    }
+
+}
