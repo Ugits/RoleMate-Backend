5a480de37ceaa2b0e09b5312aff255314a530f77
Jonas Ullvahn <ullvahn@gmail.com>
2024-12-27 16:56:39 +0100

    Add validation to CharacterDTO

--- src/main/java/org/jonas/rolemate_backend/character/model/dto/CharacterDTO.java
+++ src/main/java/org/jonas/rolemate_backend/character/model/dto/CharacterDTO.java
@@ -1,18 +1,51 @@
 package org.jonas.rolemate_backend.character.model.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.jonas.rolemate_backend.character.model.entity.CharacterEntity;
 
 public record CharacterDTO(
         Long id,
+        @NotBlank(message = "Name is required.")
         String name,
-        int level,
-        int strength,
-        int dexterity,
-        int constitution,
-        int intelligence,
-        int wisdom,
-        int charisma
-) {
+
+        @NotNull
+        @Min(value = 1, message = "Level must be at least 1.")
+        @Max(value = 20, message = "Level must be at most 9.")
+        Integer level,
+
+        @NotNull
+        @Min(value = 1, message = "Strength must be at least 1.")
+        @Max(value = 30, message = "Strength must be at most 20.")
+        Integer strength,
+
+        @NotNull
+        @Min(value = 1, message = "Dexterity must be at least 1.")
+        @Max(value = 30, message = "Dexterity must be at most 20.")
+        Integer dexterity,
+
+        @NotNull
+        @Min(value = 1, message = "Constitution must be at least 1.")
+        @Max(value = 30, message = "Constitution must be at most 20.")
+        Integer constitution,
+
+        @NotNull
+        @Min(value = 1, message = "Intelligence must be at least 1.")
+        @Max(value = 30, message = "Intelligence must be at most 20.")
+        Integer intelligence,
+
+        @NotNull
+        @Min(value = 1, message = "Wisdom must be at least 1.")
+        @Max(value = 30, message = "Wisdom must be at most 20.")
+        Integer wisdom,
+
+        @NotNull
+        @Min(value = 1, message = "Charisma must be at least 1.")
+        @Max(value = 30, message = "Charisma must be at most 20.")
+        Integer charisma
+        ) {
 
     public CharacterDTO(CharacterEntity character) {
         this(
